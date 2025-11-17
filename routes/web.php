98c6a7faaa9c6bd5bf0111de5d20e7d5f197dc04
nichraff <24k10010@student.unika.ac.id>
2025-11-17 10:49:02 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    redirect('/products');
+    return redirect('/products');
 });
 
 Route::resource('products', ProductController::class);
